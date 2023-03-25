07d5ee39cee0946bd2859ba28332757d77a2f01c
Nikolay <3kaal6@gmail.com>
2023-03-25 21:50:30 +1000

    нормализация цены, подбор параметров модели

--- test_age.py
+++ test_age.py
@@ -8,13 +8,13 @@ from catboost import CatBoostClassifier
 from sklearn.model_selection import train_test_split
 
 SPLIT_SEED = 42
-DATA_FILE = 'data_in/all_usr_emb_f80_i40.parquet'
+DATA_FILE = 'data_in/sum50_count30_countdate30_regionsumreq30_usr_emb_f_i50.parquet'
 TARGET_FILE = 'data_in/public_train.pqt'
 
 data = pq.read_table(f"{DATA_FILE}").to_pandas()
 
-data_1 = pq.read_table(f"data_in/transform_usr_emb_f80_i50.parquet").to_pandas()
-data = data.merge(data_1, how='left', on=['user_id'])
+# data_1 = pq.read_table(f"data_in/transform_usr_emb_f80_i50.parquet").to_pandas()
+# data = data.merge(data_1, how='left', on=['user_id'])
 # data_2 = pq.read_table(f"data_in/night_bas_usr_emb_f20_i50.parquet").to_pandas()
 # data = data.merge(data_2, how='left', on=['user_id'])
 
@@ -38,13 +38,21 @@ data_t_age = data_t_age[data_t_age['age'] != 'NA']
 data_t_age['age'] = data_t_age['age'].map(float)
 data_t_age['age'] = data_t_age['age'].map(age_bucket)
 data_t_age = data_t_age[data_t_age['age'] != 0]
+print(data_t_age.count())
 print(data_t_age['age'].value_counts())
+# удалим пользователь с разрывом в днях больше 109
+# data_t_age = data_t_age.loc[~data_t_age['user_id'].isin([155670, 327408, 28719, 330397, 273762, 78276, 188466, 220333, 265327, 406073])]
+# print(data_t_age.count())
+# print(data_t_age['age'].value_counts())
+# cat_features = list(data_t_age.drop('is_male', axis = 1).select_dtypes(['object']).columns)
+# print(cat_features)
 
 x_train, x_test, y_train, y_test = train_test_split(
     data_t_age.drop(['user_id', 'age', 'is_male'], axis=1), data_t_age['age'], test_size=0.25, random_state=SPLIT_SEED)
 
-clf_age = CatBoostClassifier(thread_count=3
-                             , iterations=1000
+clf_age = CatBoostClassifier(thread_count=6
+                             , iterations=500
+                             # , cat_features=cat_features
                              , random_seed=SPLIT_SEED, learning_rate=0.05, early_stopping_rounds=100
                              , eval_metric='TotalF1'
                              # , loss_function='MultiClass'
@@ -59,6 +67,6 @@ print(m.classification_report(y_test, clf_age.predict(x_test),
 
 # clf_age.get_feature_importance(prettified=True).to_csv(f'data_out/clf_age_importance.csv', index=False, mode='w')
 
-#     accuracy                           0.44     67413
-#    macro avg       0.45      0.30      0.31     67413
-# weighted avg       0.44      0.44      0.41     67413
+#     accuracy                           0.44     67410
+#    macro avg       0.44      0.29      0.29     67410
+# weighted avg       0.43      0.44      0.40     67410
